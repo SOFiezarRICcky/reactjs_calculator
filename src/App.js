e77dbc4a89f8318cd2acf3535dddea50f3bfb638
Sofiezar Ricky <sofiezar195@gmail.com>
2019-11-22 15:10:22 +0700

    Calculator React Js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import './style/styles.css'
 import './App.css';
+import Hasil from './event/hasil';
+import Tombol from './event/tombol';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    result: ''
+  };
+  buttonPressed = buttonName => {
+    if (buttonName === "=") {
+      this.calculate();
+    } else if (buttonName === "C") {
+      this.reset();
+    } else
+      this.setState({
+        result: this.state.result + buttonName
+      });
+  };
+
+  reset = () => {
+    this.setState({
+      results: ''
+    });
+  };
+
+  calculate = () => {
+    try {
+      this.setState({
+        result: eval(this.state.result)
+      });
+    } catch (e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Hasil results={this.state.result} />
+        <Tombol buttonPressed={this.buttonPressed} />
+      </div>
+    );
+  }
 }
 
 export default App;
